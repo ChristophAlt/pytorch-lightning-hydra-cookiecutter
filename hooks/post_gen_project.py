1a317087c31d9a98c8be6b0f43fadc0d7dd0ca52
Christoph Alt <christoph.alt@posteo.de>
2021-08-20 12:00:26 +0200

    Fix post_gen hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,7 +5,8 @@ import sys
 def set_python_version():
     python_version = str(sys.version_info.major) + "." + str(sys.version_info.minor)
 
-    file_names = ["Dockerfile", "Pipfile", ".github/workflows/test.yml"]
+    # file_names = ["Dockerfile", "Pipfile", ".github/workflows/test.yml"]
+    file_names = ["Dockerfile", "Pipfile"]
     for file_name in file_names:
         with open(file_name) as f:
             contents = f.read()
